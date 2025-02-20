b7514fdaf6575391c963b26e73f41511605a9de1
Umer Idrisi <muhammad.umer444@gmail.com>
2025-02-20 22:09:54 +0500

    Update contribute.tsx

--- pages/contribute.tsx
+++ pages/contribute.tsx
@@ -5,13 +5,13 @@ import PageLayout from "../components/PageLayout";
 export default function PageContribute() {
   return (
     <PageLayout
-      title="Contribute - Headly"
+      title="Contribute - TheLahoreTimes"
       description="News reader app for the critical sense."
     >
       <section className="fullscreen">
         <div className="fullscreen-container">
           <div className="container">
-            <h1>Headly</h1>
+            <h1>TheLahoreTimes</h1>
             <p>News for the critical sense</p>
           </div>
         </div>
